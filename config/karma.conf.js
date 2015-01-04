5babcfb04341c8da988edc68d43fcd8dfb93c2db
brownman <brownman556@gmail.com>
2015-01-04 16:40:29 +0200

    Update karma.conf.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['PhantomJS'],
 
     plugins : [
             'karma-junit-reporter',
